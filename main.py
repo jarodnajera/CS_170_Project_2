aaabd870b50d1e35b728a5d4c0c3e6513f1549de
Jarod Najera <jarodnajera@yahoo.com>
2022-12-08 14:03:59 -0800

    implemented forward elimination

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # Original code by Jarod Najera 862179022
 # small dataset 109
 # large dataset 64
+import nearest_neighbor as nn
+import numpy
 
 if __name__ == '__main__':
     # load data
-    print('hello world')
+    data = numpy.loadtxt('CS170_Small_Data__109.txt')
+    data = numpy.array(data)
+
+    nn.feature_search(data)
--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,55 +1,82 @@
 # nearest neighbor
-import random
 import math
 import numpy
+import copy
 
 
-def leave_one_out_cross_validation():
+def leave_one_out_cross_validation(data, current_set, feature_to_add):
     # uncomment for debugging
     # return random.randint()
-    # load data
-    data = numpy.loadtxt
 
-    correctly_classified = 0
+    # configure data to consider certain features
+    edited_data = copy.deepcopy(data)
+    for row in edited_data:
+        if current_set:
+            for column, j in enumerate(row):
+                if column != 0 and column not in current_set and column != feature_to_add:
+                    row[column] = 0
+        else:
+            for column, j in enumerate(row):
+                if column != 0 and column != feature_to_add:
+                    row[column] = 0
 
-    for i in range(len(data)):
-        object_to_classify = data
-        label_object_to_classify = data
+    number_correctly_classified = 0
+
+    for j, i in enumerate(edited_data):
+        object_to_classify = i[1:]
+        label_object_to_classify = i[0]
+
 
         nearest_neighbor_distance = float('Inf')
         nearest_neighbor_location = float('Inf')
 
-        for j in range(len(data)):
-            if j != i:
-                distance = math.sqrt(sum((object_to_classify - data)**2))
+        for l, k in enumerate(edited_data):
+            if j != l:
+                distance = math.sqrt(sum((object_to_classify - k[1:])**2))
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
-                    nearest_neighbor_location = j
-                    nearest_neighbor_label = data
+                    nearest_neighbor_location = l
+                    nearest_neighbor_label = edited_data[l][0]
         
+        #print(f'Object {j} is class {label_object_to_classify}')
+        #print(f'Its nearest neighbor is {nearest_neighbor_location} which is in class {nearest_neighbor_label}')
+
         if label_object_to_classify == nearest_neighbor_label:
-            correctly_classified += 1
+            number_correctly_classified += 1
         
-    return correctly_classified / len(data)
-
-
+    return number_correctly_classified / len(edited_data)
 
 def feature_search(data):
-    current_features = set()
+    current_set_of_features = set()
+    num_features = len(data[0][1:])
+    best_features = set()
+    best_accuracy = 0
 
-    for i in range(len(data)):
-        print(f'On the {i}-th level of the search tree')
-        feature_to_add = 0
-        best_accuracy = 0
+    print('Beginning search...')
+    for i in range(num_features):
+        #print(f'On the {i+1}-th level of the search tree')
+        feature_to_add_at_this_level = 0
+        best_so_far_accuracy = 0
 
-        for j in range(len(data)):
-            if not current_features.intersection([j]):
-                print(f'Considering adding the {j} feature...')
-                accuracy = leave_one_out_cross_validation(data, current_features, j+1)
+        for k in range(num_features):
+            if not current_set_of_features.intersection([k+1]):
+                #print(f'--Considering adding the {k+1} feature')
+                accuracy = leave_one_out_cross_validation(data, current_set_of_features, k+1)
+                print(f'Using feature(s) ({current_set_of_features}, {k+1}) accuracy is {accuracy:.3f}')
+            
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k+1
+                
+                    if best_so_far_accuracy > best_accuracy:
+                        best_accuracy = copy.deepcopy(best_so_far_accuracy)
+                        best_features = copy.deepcopy(current_set_of_features)
+                        best_features.add(feature_to_add_at_this_level)
 
-                if accuracy > best_accuracy:
-                    best_accuracy = accuracy
-                    feature_to_add = j
-        
-        current_features.add(feature_to_add)
-        print(f'On level {i}, feature {feature_to_add} was added to the current set')
+
+        current_set_of_features.add(feature_to_add_at_this_level)
+        print(f'Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy:.3f}')
+        #print(f'On level {i+1} added feature {feature_to_add_at_this_level} to current set')
+    
+    print(f'Finished search! The best feature subset is {best_features} which has an accuracy of {best_accuracy:.3f}')
+    
